461707becf35fb1f74347124721bf29b77e981dd
Scott Cardinali <scott.cardinali@gmail.com>
2023-09-24 13:37:30 -0400

    initial

---
+++ src/main/java/com/scott/seleniumRefresher/SeleniumRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.scott.seleniumRefresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumRefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumRefresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scott/seleniumRefresher/SeleniumRefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scott.seleniumRefresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumRefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
